3ebe03632861c5bc597373188669e462bd7caa9f
jbeberle <jbeberle@gmail.com>
2023-10-20 13:55:50 -0600

    Initial Commit

---
+++ src/test/java/gov/va/signalling/SignallingApplicationTests.java
@@ -0,0 +1,13 @@
+package gov.va.signalling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SignallingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
